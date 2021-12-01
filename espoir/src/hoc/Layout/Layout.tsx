dd9d7ef74e86fe68a5b1147ee70a602e86d3077a
MiVladie <justvladon@outlook.com>
2021-12-02 01:07:29 +0200

    added Layout

--- espoir/src/hoc/Layout/Layout.tsx
+++ espoir/src/hoc/Layout/Layout.tsx
@@ -5,7 +5,13 @@ import classes from './Layout.module.scss';
 import './reset.css';
 
 const Layout: React.FC = ({ children }) => {
-	return <div className={classes.Layout}>{children}</div>;
+	return (
+		<div className={classes.Layout}>
+			<div className={classes.Background} />
+
+			<div className={classes.Content}>{children}</div>
+		</div>
+	);
 };
 
 export default Layout;
